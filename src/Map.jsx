986d0ae1ba1ed004604c2daebdb402032a31e651
ZufarSyaafie <zufar.syaafie@gmail.com>
2024-10-26 07:23:22 +0700

    Change Tittle and Add Marker

--- src/Map.jsx
+++ src/Map.jsx
@@ -5,14 +5,17 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 //new marker
 import L from "leaflet";
-import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
 let DefaultIcon = L.icon({
 	iconUrl: icon,
+	shadowUrl: iconShadow,
 	iconSize: [25, 41],
 	iconAnchor: [12, 41],
 	popupAnchor: [1, -34],
 });
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 const MapComponent = (props) => {
 	return (
